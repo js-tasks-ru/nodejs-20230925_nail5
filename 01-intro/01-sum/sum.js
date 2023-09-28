8e156c6e05f6a5c99686d6378dac38e71e858318
NailGalimov <nailgalimov8@gmail.com>
2023-09-28 12:15:47 +0300

    add solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,16 @@
 function sum(a, b) {
-  /* ваш код */
+    const isANumber = typeof a === 'number'
+    const isBNumber = typeof b === 'number'
+
+    if (!isANumber) {
+        throw new TypeError(`TypeError: ${a} is not a Number`)
+    }
+
+    if (!isBNumber) {
+        throw new TypeError(`TypeError: ${b} is not a Number`)
+    }
+
+    return a + b
 }
 
 module.exports = sum;
