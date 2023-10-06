300118432d92a2467585beaee91ff3a102493a7e
NailGalimov <nailgalimov8@gmail.com>
2023-10-06 19:21:33 +0300

    СтрМодуль 3: Стримы

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.byteCount = 0;
+    this.limit = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
+    this.byteCount = this.byteCount + chunk.byteLength;
 
+    if (this.byteCount > this.limit) {
+      return callback(new LimitExceededError());
+    }
+
+    callback(null, chunk);
   }
 }
 
